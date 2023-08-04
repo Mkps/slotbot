cee6687df17940d5d4df1f664ea5fbf758796493
Alexis LOUBIERE <alex.loubiere@gmail.com>
2023-08-04 17:52:09 +0200

    Access the slot page and list the available slots

--- main.py
+++ main.py
@@ -1,28 +1,42 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select
+from selenium import webdriver #type: ignore
+from selenium.webdriver.common.by import By  #type: ignore
+from selenium.webdriver.support.wait import WebDriverWait #type: ignore
+# from selenium.webdriver.support.select import Select 
 import time
 
-username = 'toto'
-service = Service('/usr/bin/chromedriver')
-options = webdriver.ChromeOptions()
+
+username = 'username'
+password = 'password'
+project = 'project'
+dropdown_usr = '\n' + username + '\n'
+driver = webdriver.Chrome()
 url = 'https://profile.intra.42.fr'
-# url = 'https://selenium.dev'
-driver = webdriver.Chrome(service = service, options=options)
+# url = 'https://selenium.dev/selenium/web/web-form.html'
+# url = 'https://42.fr'
 driver.get(url)
+time.sleep(2)
+title = driver.title
+assert title == "Sign in to Realm for 42 students"
+driver.implicitly_wait(0.5)
+driver.find_element(By.NAME, 'username').send_keys(username)
+driver.find_element(By.NAME, 'password').send_keys(password)
+driver.find_element(By.ID, 'kc-login').click()
 time.sleep(1)
-sign_in = driver.find_element(By.ID, 'kc-login')
-username_field = driver.find_element(By.NAME, 'username')
-username_field.send_keys(username)
-pwd_field = driver.find_element(By.NAME, 'password')
-pwd_field.send_keys('pwd0123')
-sign_in.click()
-# button.click()
-# logout = driver.find_element(By.ID, 'Logout')
-# logout.click()
-dropdown = driver.find_element(By.ID, 'dropdown')
-dropdown.click()
-# history = driver.find_element(By.LINK_TEXT, "Logout")
-# history.click()
+driver.find_element(By.XPATH,"//*[contains(text(), '" + project + "')]").click()
+title = driver.title
+project_title = "Intra Projects " + project
+assert title == project_title
 time.sleep(3)
+driver.find_element(By.XPATH, "//*[contains(text(), 'Subscribe to defense')]").click()
+time.sleep(3)
+driver.find_element(By.XPATH,"//*[contains(text(), '" + dropdown_usr + "')]").click()
+time.sleep(1)
+x = 0
+slots = driver.find_elements(By.CLASS_NAME, "fc-time")
+for slot in slots:
+    start = slot.get_attribute("data-full") 
+    if start:
+        print(start)
+driver.find_element(By.XPATH, "//*[contains(text(), 'Logout')]").click()
+time.sleep(3)
+driver.quit()
