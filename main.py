5578ca1f4d8052e0305d9d9187ceb936eded0e9e
Alexis LOUBIERE <alex.loubiere@gmail.com>
2023-08-04 19:21:56 +0200

    Added input for parameters from program. Now can get the first available slot and refresh until the desired number of slot has been booked. TODO/WARNING: the bot will book the first available slot regardless of the day.

--- main.py
+++ main.py
@@ -1,42 +1,61 @@
 from selenium import webdriver #type: ignore
 from selenium.webdriver.common.by import By  #type: ignore
-from selenium.webdriver.support.wait import WebDriverWait #type: ignore
+from getpass import getpass
 # from selenium.webdriver.support.select import Select 
 import time
+def elem_xpath_text(driver, msg):
+     driver.find_element(By.XPATH,"//*[contains(text(), '" + msg + "')]").click()
 
+def calendar_refresh(driver):
+    driver.refresh()
+    time.sleep(2)
+    slots = driver.find_elements(By.CLASS_NAME, "fc-time")
+    for slot in slots:
+        start = slot.get_attribute("data-full") 
+        if start:
+            print(start)
+            slot.click()
+            time.sleep(1)
+            elem_xpath_text(driver, 'Cancel')
+            time.sleep(1)
+            break
 
-username = 'username'
-password = 'password'
-project = 'project'
-dropdown_usr = '\n' + username + '\n'
-driver = webdriver.Chrome()
-url = 'https://profile.intra.42.fr'
-# url = 'https://selenium.dev/selenium/web/web-form.html'
-# url = 'https://42.fr'
-driver.get(url)
-time.sleep(2)
-title = driver.title
-assert title == "Sign in to Realm for 42 students"
-driver.implicitly_wait(0.5)
-driver.find_element(By.NAME, 'username').send_keys(username)
-driver.find_element(By.NAME, 'password').send_keys(password)
-driver.find_element(By.ID, 'kc-login').click()
-time.sleep(1)
-driver.find_element(By.XPATH,"//*[contains(text(), '" + project + "')]").click()
-title = driver.title
-project_title = "Intra Projects " + project
-assert title == project_title
-time.sleep(3)
-driver.find_element(By.XPATH, "//*[contains(text(), 'Subscribe to defense')]").click()
-time.sleep(3)
-driver.find_element(By.XPATH,"//*[contains(text(), '" + dropdown_usr + "')]").click()
-time.sleep(1)
-x = 0
-slots = driver.find_elements(By.CLASS_NAME, "fc-time")
-for slot in slots:
-    start = slot.get_attribute("data-full") 
-    if start:
-        print(start)
-driver.find_element(By.XPATH, "//*[contains(text(), 'Logout')]").click()
-time.sleep(3)
-driver.quit()
+def exit_routine(driver):
+    dd_msg = '\n' + username + '\n'
+    elem_xpath_text(driver, dd_msg)
+    time.sleep(1)
+    elem_xpath_text(driver, 'Logout')
+    time.sleep(1)
+    driver.quit()
+
+def bot_routine(username, password, project, nb_slot):
+    driver = webdriver.Chrome()
+    url = 'https://profile.intra.42.fr'
+    driver.get(url)
+    time.sleep(2)
+    title = driver.title
+    assert title == "Sign in to Realm for 42 students"
+    driver.implicitly_wait(0.5)
+    driver.find_element(By.NAME, 'username').send_keys(username)
+    driver.find_element(By.NAME, 'password').send_keys(password)
+    driver.find_element(By.ID, 'kc-login').click()
+    time.sleep(1)
+    driver.find_element(By.XPATH,"//*[contains(text(), '" + project + "')]").click()
+    title = driver.title
+    project_title = "Intra Projects " + project
+    assert title == project_title
+    time.sleep(3)
+    d_msg = 'Subscribe to defense'
+    elem_xpath_text(driver, d_msg)
+    x = 0
+    while x < nb_slot:
+        calendar_refresh(driver)
+        x += 1
+    time.sleep(1)
+    exit_routine(driver)
+
+project = input("Enter project name: ")
+nb_slot = input("How many slot do you need?")
+username = input("Enter your username: ")
+password = getpass("Enter your password: ")
+bot_routine(username, password, project, nb_slot)
